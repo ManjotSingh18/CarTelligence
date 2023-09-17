469cbc6183816e7a653372e838d5543eb6e2fe30
ManjotSingh18 <manjos2@uci.edu>
2023-09-17 00:22:22 -0700

    Error page implemented

--- cars/views.py
+++ cars/views.py
@@ -14,32 +14,35 @@ from django.views.decorators.csrf import csrf_exempt
 # Create your views here.
 @csrf_exempt
 def home(request):
-    if request.method == "POST":
-        form= CarForm(request.POST, auto_id="Placer")
-        if form.is_valid():
-            files = request.FILES
-            image = files.get("pic")
-            instance = Car()
-            instance.pic = image
-            if 'media' in os.listdir(".") and len(os.listdir("./media/cars/")) >= 1:
-                files = glob.glob('./media/cars/*')
-                for f in files:
-                    os.remove(f)
-            instance.save()
-            time.sleep(2)
-            try:
-                filename=os.listdir("./media/cars/")[0]
-            except:
-                return render(request, "cars/landing.html", {"form": form, "prediction": "", "error": "File not given or not .JPG"})
-            if (filename[-3:] == "jpg" or filename[-3:]=="JPG" or filename[-4:] == "jpeg" or filename[-4:] == "JPEG") and ('media' in os.listdir(".") and len(os.listdir("./media/cars/")) >= 1):
-                p=activate(f"./media/cars/{filename}")
-                return render(request, "cars/carpred.html", {"pico": filename, "car": p})
-            else:
-                return render(request, "cars/landing.html", {"form": form, "prediction": "", "error": "File not given or not .JPG"})
+    try:
+        if request.method == "POST":
+            form= CarForm(request.POST, auto_id="Placer")
+            if form.is_valid():
+                files = request.FILES
+                image = files.get("pic")
+                instance = Car()
+                instance.pic = image
+                if 'media' in os.listdir(".") and len(os.listdir("./media/cars/")) >= 1:
+                    files = glob.glob('./media/cars/*')
+                    for f in files:
+                        os.remove(f)
+                instance.save()
+                time.sleep(2)
+                try:
+                    filename=os.listdir("./media/cars/")[0]
+                except:
+                    return render(request, "cars/landing.html", {"form": form, "prediction": "", "error": "File not given or not .JPG"})
+                if (filename[-3:] == "jpg" or filename[-3:]=="JPG" or filename[-4:] == "jpeg" or filename[-4:] == "JPEG") and ('media' in os.listdir(".") and len(os.listdir("./media/cars/")) >= 1):
+                    p=activate(f"./media/cars/{filename}")
+                    return render(request, "cars/carpred.html", {"pico": filename, "car": p})
+                else:
+                    return render(request, "cars/landing.html", {"form": form, "prediction": "", "error": "File not given or not .JPG"})
 
-    else:
-        form = CarForm(auto_id="Placer")
-        return render(request, "cars/landing.html", {"form": form, "prediction": "", "error": ""})
+        else:
+            form = CarForm(auto_id="Placer")
+            return render(request, "cars/landing.html", {"form": form, "prediction": "", "error": ""})
+    except:
+        return render(request, "cars/error.html", {})
 
 
 
